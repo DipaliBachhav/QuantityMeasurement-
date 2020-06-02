f5f437ffb225ac79bf99323fed91ce1ebc9a311e
DipaliBachhav <dipa2718@gmail.com>
2020-06-02 23:08:10 +0530

    Added Feature To Check Feet Value is Null Throws An Exception

--- src/main/java/com/Measurement/QuantityMeasurement.java
+++ src/main/java/com/Measurement/QuantityMeasurement.java
@@ -1,7 +1,9 @@
 package com.Measurement;
 
 public class QuantityMeasurement {
-    public boolean compare(MeasurementComparator measurementComparator1,MeasurementComparator measurementComparator2){
+    public boolean compare(MeasurementComparator measurementComparator1,MeasurementComparator measurementComparator2) throws QuantityMeasurementException {
+        if (measurementComparator1 == null || measurementComparator2 == null)
+            throw new QuantityMeasurementException(QuantityMeasurementException.ExceptionType.NULL_POINTER_EXCEPTION, "Null Object");
         return measurementComparator1.equals(measurementComparator2);
     }
 }
--- src/test/java/com/Measurement/QuantityMeasurementTest.java
+++ src/test/java/com/Measurement/QuantityMeasurementTest.java
@@ -8,9 +8,23 @@ public class QuantityMeasurementTest {
 
     @Test
     public void givenFeetAndLengthIsZero_shouldCompareThen_ReturnTrue() {
-        MeasurementComparator measurementComparator1 = new MeasurementComparator(0,Length.FEET);
-        MeasurementComparator measurementComparator2 = new MeasurementComparator(0,Length.FEET);
-        boolean result = quantityMeasurement.compare(measurementComparator1,measurementComparator2);
-        Assert.assertEquals(true,result);
+        try {
+            MeasurementComparator measurementComparator1 = new MeasurementComparator(0, Length.FEET);
+            MeasurementComparator measurementComparator2 = new MeasurementComparator(0, Length.FEET);
+            boolean result = quantityMeasurement.compare(measurementComparator1, measurementComparator2);
+            Assert.assertEquals(true, result);
+        } catch (QuantityMeasurementException e) {
+        }
+    }
+
+    @Test
+    public void givenFeetValueIsNull_shouldThrowsException() {
+        MeasurementComparator converteInch = new MeasurementComparator(0, Length.FEET);
+        quantityMeasurement = new QuantityMeasurement();
+        try {
+            boolean result = quantityMeasurement.compare(converteInch, null);
+        } catch (QuantityMeasurementException e) {
+            Assert.assertEquals(QuantityMeasurementException.ExceptionType.NULL_POINTER_EXCEPTION, e.type);
+        }
     }
 }
