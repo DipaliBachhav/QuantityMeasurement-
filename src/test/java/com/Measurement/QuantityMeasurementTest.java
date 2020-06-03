bf696201f47e2255a97f75a25abdb4ebb81eff98
DipaliBachhav <dipa2718@gmail.com>
2020-06-03 15:34:30 +0530

    Added Feature To Check 36Inch Is Equals To 1Yard

--- src/test/java/com/Measurement/QuantityMeasurementTest.java
+++ src/test/java/com/Measurement/QuantityMeasurementTest.java
@@ -190,4 +190,15 @@ public class QuantityMeasurementTest {
         } catch (QuantityMeasurementException e) {
         }
     }
+
+    @Test
+    public void givenInchAndYard_When36InchEqualsTo1Yard_ThenReturnTrue(){
+        try {
+            MeasurementComparator measurementComparator1 = new MeasurementComparator(36, Length.INCH);
+            MeasurementComparator measurementComparator2 = new MeasurementComparator(1, Length.YARD);
+            boolean result = quantityMeasurement.compare(measurementComparator1, measurementComparator2);
+            Assert.assertEquals(true, result);
+        }catch (QuantityMeasurementException e) {
+        }
+    }
 }
