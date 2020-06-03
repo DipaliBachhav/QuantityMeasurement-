977839ceb2cdffecd6d2c0247aa1e610a429a2be
DipaliBachhav <dipa2718@gmail.com>
2020-06-03 15:37:43 +0530

    Added Feature To Check 1Yard Is Equals To 3Feet

--- src/test/java/com/Measurement/QuantityMeasurementTest.java
+++ src/test/java/com/Measurement/QuantityMeasurementTest.java
@@ -201,4 +201,15 @@ public class QuantityMeasurementTest {
         }catch (QuantityMeasurementException e) {
         }
     }
+
+    @Test
+    public void givenFeetAndYard_When1YardEqualsTo3Feet_ThenReturnTrue(){
+        try {
+            MeasurementComparator measurementComparator1 = new MeasurementComparator(3, Length.FEET);
+            MeasurementComparator measurementComparator2 = new MeasurementComparator(1, Length.YARD);
+            boolean result = quantityMeasurement.compare(measurementComparator1, measurementComparator2);
+            Assert.assertEquals(true, result);
+        }catch (QuantityMeasurementException e) {
+        }
+    }
 }
